978b33392c0519e529609559e44490718a8868d7
razieh7 <rmrady758@gmail.com>
2024-02-03 17:02:47 +0330

    finish-question-box

--- question-box/src/App.jsx
+++ question-box/src/App.jsx
@@ -1,34 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const box1 = useRef()
+  const box2 = useRef()
+  const box3 = useRef()
+  const plus1 = useRef()
+  const mins = useRef()
+  function show1(){
+    box1.current.classList.toggle('h-[180px]')
+    plus1.current.classList.toggle('hidden')
+    mins.current.classList.toggle('hidden')
+  }
+  function show2(){
+    box2.current.classList.toggle('h-[180px]')
+  }
+  function show3(){
+    box3.current.classList.toggle('h-[180px]')
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className='w-full h-[100vh] bg-slate-200 flex flex-col  items-center pt-40'>
+      <div className='w[80%] flex flex-col items-center'>
+        <h2 className='text-[40px] font-mono font-black text-[#936a24]'>general questions</h2>
+
+
+        {/* question-box 1 */}
+        <div ref={box1} className='w-[60%] h-[70px] bg-white rounded-md overflow-hidden p-7 mt-7 transitionR'>
+          <h2 onClick={show1} className='texy-[20px] font-bold border-b border-gray-400 text-[#131154] flex justify-between cursor-pointer pb-5'>
+            do you accept all major credit cards? 
+          <span ref={plus1}  className='text-[17px]  font-black  text-[#936a24] border-2 rotat border-[#936a24] px-2 hover:rotate-90 transitionR'>+</span>
+          <span ref={mins}  className='text-[17px] hidden font-black  text-[#936a24] border-2 rotat border-[#936a24] px-2 transitionR'>-</span>
+          </h2>
+          <p className=' text-gray-500 pt-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolore illo dolores quia nemo doloribus quaerat, magni numquam repellat reprehenderit.</p>
+        </div>
+
+
+        {/* question-box 2 */}
+        <div ref={box2} className='w-[60%] h-[70px] bg-white rounded-md overflow-hidden p-7 mt-7 transitionR'>
+          <h2 onClick={show2} className='texy-[20px] font-bold border-b border-gray-400 text-[#131154] flex justify-between pb-5'>do you suppport local farmers? <span className='text-[17px] font-black px-2  text-[#936a24] border-2 border-[#936a24] hover:rotate-90 transitionR'>+</span></h2>
+          <p className=' text-gray-500 pt-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolore illo dolores quia nemo doloribus quaerat, magni numquam repellat reprehenderit.</p>
+        </div>
+
+
+        {/* question-box 3 */}
+        <div ref={box3} className='w-[60%] h-[70px] bg-white rounded-md overflow-hidden p-7 mt-7 transitionR'>
+          <h2 onClick={show3} className='texy-[20px] font-bold border-b border-gray-400 text-[#131154] flex justify-between pb-5'>do you use organic ingredients? <span className='text-[17px] font-black px-2  text-[#936a24] border-2 border-[#936a24] hover:rotate-90 transitionR'>+</span></h2>
+          <p className=' text-gray-500 pt-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est dolore illo dolores quia nemo doloribus quaerat, magni numquam repellat reprehenderit.</p>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
