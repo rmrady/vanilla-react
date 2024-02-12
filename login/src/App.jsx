5a7fcb5c6870a5aa0319dfc497e0ad9ce3f3495b
razieh7 <rmrady758@gmail.com>
2024-02-12 23:54:18 +0330

    login

---
+++ login/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './index.css';
+import React from 'react'
+import Section from './component/section'
+
+
+
+function App() {
+  return (
+    <body>
+      <Section />
+    </body>
+  )
+}
+
+export default App
+
+
---
+++ login/src/component/section.jsx
@@ -0,0 +1,320 @@
+import React, { useState } from 'react'
+
+function Section() {
+    const [data , setData] = useState([]);
+    function add() {
+        let user = document.getElementById('user').value;
+        let pass = document.getElementById('pass').value;
+        let check = document.getElementById('check');
+
+        if(user == '' && pass == ''){
+            console.log('error');
+        }else{
+            const newTask = {
+                username: user,
+                password: pass,
+            };
+    
+            fetch('https://653fe47e45bedb25bfc1697b.mockapi.io/myapi/login1/users', {
+                method: 'POST',
+                headers: { 'content-type': 'application/json' },
+                body: JSON.stringify(newTask)
+            }).then(res => {
+                if (res.ok) {
+                    return res.json();
+                }
+    
+            }).then(task => {
+                console.log('added new');
+                check.style.display = "block";
+                setTimeout(() => {
+                    check.style.display = "none";
+    
+                }, 2000)
+    
+            }).catch(error => {
+    
+            })
+
+        }
+        
+        document.getElementById('user').value = "";
+        document.getElementById('pass').value = "";
+
+    }
+
+    return (
+        <section>
+
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+
+
+
+            <div className="signin">
+                <div className="content">
+                    <h2>Sign In</h2>
+                    <div className="form">
+                        <div className="inputBx">
+                            <input id="user" type="text" required />
+                            <i>Username</i>
+                        </div>
+                        <div className="inputBx">
+                            <input id="pass" type="password" required />
+                            <i>Password</i>
+                        </div>
+                        <div className="links">
+                            <a href="#">Forgot Password</a>
+                            <a href="#">Signup</a>
+                        </div>
+                        <div className="inputBx">
+                            <input onClick={add} type="submit" value="Login" />
+                            <i className="bi bi-check-lg" id="check"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    )
+}
+
+export default Section
